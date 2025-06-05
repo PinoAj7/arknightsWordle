90c9a7b9e8fb94806966acb161ab3760452fb381
PinoAj7 <pinoaj7@gmail.com>
2025-06-05 14:15:02 +0000

    si

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ScoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', [AuthController::class, 'me'])->middleware('auth:sanctum');
